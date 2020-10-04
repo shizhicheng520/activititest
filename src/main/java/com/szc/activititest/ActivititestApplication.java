ddab1463a35578de1bff48e3452efc8d6ba22464
lenovo <shizhichengs@163.com>
2020-10-04 10:37:54 +0800

    Initial commit

---
+++ src/main/java/com/szc/activititest/ActivititestApplication.java
@@ -0,0 +1,13 @@
+package com.szc.activititest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivititestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivititestApplication.class, args);
+    }
+
+}
